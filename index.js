e9bd793d9e0c2bfde41d4ae1868f600a9f98d443
kata <kata.choi@gmail.com>
2017-06-12 15:45:41 +0800

    .

--- index.js
+++ index.js
@@ -171,11 +171,7 @@ class ChromeJS {
     return new Promise(async(resolve, reject) => {
       let documentNode = await this.client.DOM.getDocument()
       let clickNode = await this.client.DOM.querySelector({nodeId: documentNode.root.nodeId, selector: selector})
-      this.client.DOM.getBoxModel({nodeId: clickNode.nodeId}).then((boxModel) => {
-        resolve(boxModel)
-      }).catch((e) => {
-        reject(e)
-      })
+      this.client.DOM.getBoxModel({nodeId: clickNode.nodeId}).then(resolve).catch(reject)
     })
   }
 
